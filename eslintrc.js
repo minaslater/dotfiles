2e51e3e0f3ecc8721c6a500e8b557d1f9218962a
Mina Slater <minaslater@BusterBot.local>
2018-05-07 19:37:47 -0500

    initial commit

---
+++ eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "rules": {
+    "one-var": ["error", "always"],
+    "valid-jsdoc": "off",
+    "ignoreComments": "off",
+    "ignoreStrings": "off",
+    "max-len": "off",
+    "require-jsdoc": "off",
+    "quotes": ["error", "double"],
+    "no-multi-spaces": "off"
+  },
+  "extends": "eslint-config-google"
+};
+
